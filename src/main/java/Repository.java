9ac572cb8988387e3aa3dcc83068fdd8694a1c13
CatAnderson <thecatanderson100@gmail.com>
2020-11-21 11:13:48 +0000

    RepoType enum setup, test setup for Repository

--- src/main/java/Repository.java
+++ src/main/java/Repository.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Repository {
+
+    private String name;
+    private String description;
+    private RepoType repoType;
+    private ArrayList<Commit> commits;
+
+    public Repository(String name, String description, RepoType repoType){
+        this.name = name;
+        this.description = description;
+        this.repoType = repoType;
+    }
 }
--- src/test/java/RepositoryTest.java
+++ src/test/java/RepositoryTest.java
@@ -1,2 +1,15 @@
+import org.junit.Before;
+
 public class RepositoryTest {
+
+    private Commit commit;
+    private Repository repository;
+
+    @Before
+    public void before(){
+        commit = new Commit("added some css styling", "2de56ffs781fc99dh9");
+        repository = new Repository("Java_Github_Modelling", "make a representation of github using Java", RepoType.PUBLIC);
+    }
+
+
 }
