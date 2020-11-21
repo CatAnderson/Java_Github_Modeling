1d9135c6e2447d9c08d89690a90cc075f37bd9be
CatAnderson <thecatanderson100@gmail.com>
2020-11-21 13:04:24 +0000

    setup for account files

--- src/test/java/GithubAccountTest.java
+++ src/test/java/GithubAccountTest.java
@@ -1,3 +1,5 @@
+import org.junit.Before;
+
 public class GithubAccountTest {
 
     private Commit commit1;
@@ -7,6 +9,18 @@ public class GithubAccountTest {
     private Repository repo2;
 
     private GithubAccount account;
+
+    @Before
+    public void before(){
+        commit1 = new Commit("added some css styling", "2de56ffs781fc99dh9");
+        commit2 = new Commit("added some css styling & html", "2de56d44o2483302j3");
+        repo1 = new Repository("Java_Github_Modelling", "make a representation of github using Java", RepoType.PUBLIC);
+        repo2 = new Repository("Watch_this_Space", "space app", RepoType.PRIVATE);
+        account = new GithubAccount("CatAnderson", "CA1990", AccountType.FREE);
+
+        repo1.addCommit(commit1);
+        repo1.addCommit(commit2);
+    }
 
 
 }
